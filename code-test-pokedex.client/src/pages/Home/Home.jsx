5276f89bcd6e748a91feb32defa07e2524c312f5
fridish <your_email@example.com>
2024-12-15 11:18:24 +0100

    removed incorrect code

--- code-test-pokedex.client/src/pages/Home/Home.jsx
+++ code-test-pokedex.client/src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     );
 
     const fetchRandomPokemon = () => {
-        const id = Math.floor(Math.random() * 1024) + 1000;
+        const id = Math.floor(Math.random() * 1024) + 1;
         setRandomId(id);
     };
 if (pokemon){
